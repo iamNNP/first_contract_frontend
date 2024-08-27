1f61edc7f757837d157392be95dfabe008421be0
iamNNP <alexander.markovskiy@mail.ru>
2024-08-27 18:17:48 +0300

    Manifest url fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://iamNNP.github.io/first_contract_frontend/public/tonconnect-manifest.json';
+const manifestUrl = 'https://iamnnp.github.io/first_contract_frontend/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
